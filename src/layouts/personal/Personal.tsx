f4065281de06c7453b79ea2c7062123a21c57127
Denis Balushkin <d.balushkin@aristos.pw>
2021-10-22 03:12:23 +0300

    Добавил блок Personal

---
+++ src/layouts/personal/Personal.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './Personal.module.css'
+
+const Personal:React.FC = () => {
+    return (
+        <div className={styles.personalLayout}></div>
+    )
+}
+
+export default Personal
